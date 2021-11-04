2cb7c4d86b2121d15e16cd6870f6453457f20b76
JefersonBatista <jeferson.de.oliveira.batista@gmail.com>
2021-11-04 15:58:48 -0300

    Embaralhando e virando as cartas.

--- scripts/script.js
+++ scripts/script.js
@@ -1,32 +1,76 @@
-// Main prompt message
-const msg = "Com quantas cartas você quer jogar?";
+initialize_game();
 
-// Requirement prompt message
-const r_msg = "O número de cartas deve ser par e estar entre 4 e 14.";
+function initialize_game() {
+  // Main prompt message
+  const msg = "Com quantas cartas você quer jogar?";
 
-// Number of cards of the game
-let num_cards = parseInt(prompt(msg));
+  // Requirement prompt message
+  const req_msg = "O número de cartas deve ser par e estar entre 4 e 14.";
 
-while (num_cards % 2 !== 0 || num_cards < 4 || num_cards > 14) {
-  num_cards = parseInt(prompt(r_msg + "\n" + msg));
-}
+  // Number of cards of the game
+  let num_cards = parseInt(prompt(msg));
 
-// Number of pairs of the game
-const num_pairs = num_cards / 2;
+  while (num_cards % 2 !== 0 || num_cards < 4 || num_cards > 14) {
+    num_cards = parseInt(prompt(req_msg + "\n" + msg));
+  }
 
-// Including the cards in the game
-const card_html = `<div class="card">
-  <img src="images/card-cover.png" alt="Card Cover" />
-</div>`;
+  // Number of pairs of the game
+  const num_pairs = num_cards / 2;
 
-const row1 = document.querySelector(".game .card-row#row1");
-row1.innerHTML = "";
-for (let i = 0; i < num_pairs; i++) {
-  row1.innerHTML += card_html;
-}
+  // HTML code of each card
+  function card_html(card_index) {
+    return `
+    <div class="card">
+      <img class="face back-face" src="images/card-cover.png" alt="Card Cover" />
+      <img class="face front-face" src="images/${game_cards[card_index][0]}" alt="Card Image" />
+    </div>`;
+  }
 
-const row2 = document.querySelector(".game .card-row#row2");
-row2.innerHTML = "";
-for (let i = 0; i < num_pairs; i++) {
-  row2.innerHTML += card_html;
+  function random_number_comparator(card_a, card_b) {
+    return card_a[1] - card_b[1];
+  }
+
+  // Shuffling the cards
+  const all_cards = [
+    ["bobrossparrot.gif", Math.random()],
+    ["bobrossparrot.gif", Math.random()],
+    ["explodyparrot.gif", Math.random()],
+    ["explodyparrot.gif", Math.random()],
+    ["fiestaparrot.gif", Math.random()],
+    ["fiestaparrot.gif", Math.random()],
+    ["metalparrot.gif", Math.random()],
+    ["metalparrot.gif", Math.random()],
+    ["revertitparrot.gif", Math.random()],
+    ["revertitparrot.gif", Math.random()],
+    ["tripletsparrot.gif", Math.random()],
+    ["tripletsparrot.gif", Math.random()],
+    ["unicornparrot.gif", Math.random()],
+    ["unicornparrot.gif", Math.random()],
+  ];
+
+  const game_cards = all_cards.slice(0, num_cards);
+  game_cards.sort(random_number_comparator);
+  console.log(game_cards);
+
+  let card_index = 0;
+
+  const row1 = document.querySelector(".game .card-row#row1");
+  row1.innerHTML = "";
+  let row_index = 0;
+
+  while (row_index < num_pairs) {
+    row1.innerHTML += card_html(card_index);
+    row_index += 1;
+    card_index += 1;
+  }
+
+  const row2 = document.querySelector(".game .card-row#row2");
+  row2.innerHTML = "";
+  row_index = 0;
+
+  while (row_index < num_pairs) {
+    row2.innerHTML += card_html(card_index);
+    row_index += 1;
+    card_index += 1;
+  }
 }
